c178b3177b561e42c9fe7ca2cdcda1e021ec60a2
Matthew Ratzke <matt@degree9.io>
2019-09-12 15:45:07 -0600

    Update tooltip.cljs

--- src/uikit_hl/tooltip.cljs
+++ src/uikit_hl/tooltip.cljs
@@ -1,7 +1,21 @@
 (ns uikit-hl.tooltip
   (:require [hoplon.core :as h]
-            ["uikit" :as uikit]))
+            [hoplon.jquery]
+            [uikit-hl.core :as uk]))
 
-(defmethod h/do! :uk-tooltip
+(defmulti uk-tooltip! h/kw-dispatcher :default ::default)
+
+(defmethod h/do! ::default
+  [elem key val]
+  (uk-tooltip! elem key val))
+
+(defn- format-tooltip [tooltip]
+  (str "uk-tooltip-" tooltip))
+
+(defmethod uk-tooltip! ::default
+  [elem kw v]
+  (elem :class {(format-tooltip (name kw)) v}))
+
+(defmethod uk-tooltip! ::tooltip
   [elem _ v]
-  (.tooltip uikit elem (clj->js v)))
+  (.tooltip uk/uikit elem (clj->js v)))
