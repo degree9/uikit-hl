d40b4af5fd64aa2b17f1c8ad02e3c4254cc62a09
Nolan Jelley <nolanbjelley@gmail.com>
2019-11-14 17:17:58 -0700

    Update divider.cljs

--- src/uikit_hl/divider.cljs
+++ src/uikit_hl/divider.cljs
@@ -1,4 +1,18 @@
 (ns uikit-hl.divider
   (:require [hoplon.core :as h]
-            [hoplon.jquery]
-            [uikit-hl.core :as uk]))
+            [hoplon.jquery]))
+
+(defn- format-divider [divider]
+  (str "uk-divider" divider))
+
+(defmethod h/do! ::default
+  [elem kw v]
+  (h/do! elem :class {(format-divider (name kw)) v})
+
+  (h/defelem divider [{:keys [icon small vertical] :as attr} kids]
+    (h/hr
+      (dissoc attr :icon :small :vertical)
+      ::icon     icon
+      ::small    small
+      ::vertical vertical
+      kids)))
