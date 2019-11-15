8c4904973fd4cc0e2bfedfed09c55990c282a0a1
Nolan Jelley <nolanbjelley@gmail.com>
2019-11-14 21:16:57 -0700

    Update svg.cljs

--- src/uikit_hl/svg.cljs
+++ src/uikit_hl/svg.cljs
@@ -2,3 +2,13 @@
   (:require [hoplon.core :as h]
             [hoplon.jquery]
             [uikit-hl.core :as uk]))
+
+(defmulti uk-svg! h/kw-dispatcher :default ::default)
+
+(defmethod h/do! ::default
+  [elem key val]
+  (uk-svg! elem key val))
+
+(defmethod uk-svg! ::default
+  [elem kw v]
+  (.svg uk/uikit elem (clj->js v)))
