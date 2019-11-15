01e48524217b383e6dca8fd132b404b4a531a31b
Nolan Jelley <nolanbjelley@gmail.com>
2019-11-14 17:30:14 -0700

    Update image.cljs

--- src/uikit_hl/image.cljs
+++ src/uikit_hl/image.cljs
@@ -2,3 +2,20 @@
   (:require [hoplon.core :as h]
             [hoplon.jquery]
             [uikit-hl.core :as uk]))
+
+(defmulti uk-image! h/kw-dispatcher :default ::default)
+
+(defmethod h/do! ::default
+  [elem key val]
+  (uk-image! elem key val))
+
+(defn- format-image [image]
+  (str "uk-image-" image))
+
+(defmethod uk-image! ::default
+  [elem kw v]
+  (h/do! elem :class {(format-image (name kw)) v}))
+
+(defmethod h/do! ::image
+  [elem _ v]
+  (.img uk/uikit elem (clj->js v)))
