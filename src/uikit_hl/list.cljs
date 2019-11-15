f701574f6dd66efa8c07df14e6b5d568c5ea88e9
Nolan Jelley <nolanbjelley@gmail.com>
2019-11-14 17:49:15 -0700

    Update list.cljs

--- src/uikit_hl/list.cljs
+++ src/uikit_hl/list.cljs
@@ -1,4 +1,20 @@
 (ns uikit-hl.list
+  (:refer-clojure :exclude [list])
   (:require [hoplon.core :as h]
-            [hoplon.jquery]
-            [uikit-hl.core :as uk]))
+            [hoplon.jquery]))
+
+(defn- format-list [list]
+  (str "uk-list-" list))
+
+(defmethod h/do! ::default
+  [elem kw v]
+  (h/do! elem :class {(format-list (name kw)) v})
+
+  (h/defelem list [{:keys [bullet divider striped large] :as attr} kids]
+    (h/hr
+      (dissoc attr :bullet :divider :striped :large)
+      ::bullet  bullet
+      ::divider divider
+      ::striped striped
+      ::large   large
+      kids)))
