d9df065a44ebecde7741a8868928b696690430c7
Nolan Jelley <nolanbjelley@gmail.com>
2019-11-14 21:13:17 -0700

    Update subnav.cljs

--- src/uikit_hl/subnav.cljs
+++ src/uikit_hl/subnav.cljs
@@ -1,4 +1,12 @@
 (ns uikit-hl.subnav
-  (:require [hoplon.core :as h]
-            [hoplon.jquery]
-            [uikit-hl.core :as uk]))
+  (:require [clojure.string :as s]
+            [hoplon.core :as h]
+            [hoplon.jquery]))
+
+(defn- format-subnav [subnav]
+  (str "uk-subnav-" subnav))
+
+
+(defmethod h/do! ::default
+  [elem kw v]
+  (h/do! elem :class {(format-subnav (name kw)) v}))
