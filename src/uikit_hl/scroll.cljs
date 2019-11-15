b897c701e4ea0139897cd27b9c7ac16e0f9e2ded
Nolan Jelley <nolanbjelley@gmail.com>
2019-11-14 18:10:29 -0700

    Update scroll.cljs

--- src/uikit_hl/scroll.cljs
+++ src/uikit_hl/scroll.cljs
@@ -2,3 +2,13 @@
   (:require [hoplon.core :as h]
             [hoplon.jquery]
             [uikit-hl.core :as uk]))
+
+(defmulti uk-scroll! h/kw-dispatcher :default ::default)
+
+(defmethod h/do! ::default
+  [elem key val]
+  (uk-scroll! elem key val))
+
+(defmethod uk-scroll! ::default
+  [elem kw v]
+  (.scroll uk/uikit elem (clj->js v)))
