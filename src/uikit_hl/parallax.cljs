e8f41c81adbc69213d1b0c44e87ae7ae987d4b40
Nolan Jelley <nolanbjelley@gmail.com>
2019-11-14 17:58:43 -0700

    Update parallax.cljs

--- src/uikit_hl/parallax.cljs
+++ src/uikit_hl/parallax.cljs
@@ -2,3 +2,20 @@
   (:require [hoplon.core :as h]
             [hoplon.jquery]
             [uikit-hl.core :as uk]))
+
+(defmulti uk-parallax! h/kw-dispatcher :default ::default)
+
+(defmethod h/do! ::default
+  [elem key val]
+  (uk-parallax! elem key val))
+
+(defn- format-parallax [parallax]
+  (str "uk-parallax-" parallax))
+
+(defmethod uk-parallax! ::default
+  [elem kw v]
+  (h/do! elem :class {(format-parallax (name kw)) v}))
+
+(defmethod h/do! ::parallax
+  [elem _ v]
+  (.parallax uk/uikit elem (clj->js v)))
