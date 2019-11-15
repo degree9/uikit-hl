8482dc5bb6df5ffd3f76b1fe68d05b75167393bf
Nolan Jelley <nolanbjelley@gmail.com>
2019-11-14 17:43:28 -0700

    Update lightbox.cljs

--- src/uikit_hl/lightbox.cljs
+++ src/uikit_hl/lightbox.cljs
@@ -2,3 +2,13 @@
   (:require [hoplon.core :as h]
             [hoplon.jquery]
             [uikit-hl.core :as uk]))
+
+(defmulti uk-lightbox! h/kw-dispatcher :default ::default)
+
+(defmethod h/do! ::default
+  [elem key val]
+  (uk-lightbox! elem key val))
+
+(defmethod uk-lightbox! ::default
+  [elem kw v]
+  (.lightboxPanel uk/uikit elem (clj->js v)))
