90d8260ff46b2cfc4800f495922ba2820b3a07c1
Nolan Jelley <nolanbjelley@gmail.com>
2019-11-14 17:11:30 -0700

    Update column.cljs

--- src/uikit_hl/column.cljs
+++ src/uikit_hl/column.cljs
@@ -1,4 +1,15 @@
 (ns uikit-hl.column
-  (:require [hoplon.core :as h]
-            [hoplon.jquery]
-            [uikit-hl.core :as uk]))
+  (:require [clojure.string :as s]
+            [hoplon.core :as h]
+            [hoplon.jquery]))
+
+(defn- format-column [column]
+  (-> (str "uk-" column)
+    (s/replace "-s" "@s")
+    (s/replace "-m" "@m")
+    (s/replace "-l" "@l")
+    (s/replace "-xl" "@xl")))
+
+(defmethod h/do! ::default
+  [elem kw v]
+  (h/do! elem :class {(format-column (name kw)) v}))
