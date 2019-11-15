988e043aa8f6e6bf5b24d33772418e07b3611e05
Nolan Jelley <nolanbjelley@gmail.com>
2019-11-14 17:56:24 -0700

    Update overlay.cljs

--- src/uikit_hl/overlay.cljs
+++ src/uikit_hl/overlay.cljs
@@ -2,3 +2,20 @@
   (:require [hoplon.core :as h]
             [hoplon.jquery]
             [uikit-hl.core :as uk]))
+
+(defmulti uk-overlay! h/kw-dispatcher :default ::default)
+
+(defmethod h/do! ::default
+  [elem key val]
+  (uk-overlay! elem key val))
+
+(defn- format-overlay [overlay]
+  (str "uk-overlay-" overlay))
+
+(defmethod uk-overlay! ::default
+  [elem kw v]
+  (h/do! elem :class {(format-overlay (name kw)) v}))
+
+(defmethod h/do! ::overlay
+  [elem _ v]
+  (h/do! elem :class {:uk-overlay v}))
