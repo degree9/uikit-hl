1826ff174d5aee96dc159307d96bc03fd6a9a595
Nolan Jelley <nolanbjelley@gmail.com>
2019-11-14 18:12:25 -0700

    Update scrollspy.cljs

--- src/uikit_hl/scrollspy.cljs
+++ src/uikit_hl/scrollspy.cljs
@@ -2,3 +2,13 @@
   (:require [hoplon.core :as h]
             [hoplon.jquery]
             [uikit-hl.core :as uk]))
+
+(defmulti uk-scrollspy! h/kw-dispatcher :default ::default)
+
+(defmethod h/do! ::default
+  [elem key val]
+  (uk-scrollspy! elem key val))
+
+(defmethod uk-scrollspy! ::default
+  [elem kw v]
+  (.scrollspyNav uk/uikit elem (clj->js v)))
