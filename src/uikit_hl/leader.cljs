cfc1a04f459820c312760ac9e10b3673ae57cf64
Nolan Jelley <nolanbjelley@gmail.com>
2019-11-14 17:40:56 -0700

    Update leader.cljs

--- src/uikit_hl/leader.cljs
+++ src/uikit_hl/leader.cljs
@@ -2,3 +2,13 @@
   (:require [hoplon.core :as h]
             [hoplon.jquery]
             [uikit-hl.core :as uk]))
+
+(defmulti uk-leader! h/kw-dispatcher :default ::default)
+
+(defmethod h/do! ::default
+  [elem key val]
+  (uk-leader! elem key val))
+
+(defmethod uk-leader! ::default
+  [elem kw v]
+  (.leader uk/uikit elem (clj->js v)))
