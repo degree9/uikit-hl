15e6759fb678d5216633e7dbb89edc3c5c985d79
Nolan Jelley <nolanbjelley@gmail.com>
2019-11-20 20:06:23 -0700

    Update icon.cljs

--- src/uikit_hl/icon.cljs
+++ src/uikit_hl/icon.cljs
@@ -1,40 +1,23 @@
 (ns uikit-hl.icon
-  (:require [hoplon.core :as hl]
-            [uikit-hl.core :as core]
-            ["uikit" :as uikit]
+  (:require [hoplon.core :as h]
+            [uikit-hl.core :as uk]
             ["uikit/dist/js/uikit-icons" :as icons]))
 
 ;; Init UIKit Icons ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(icons uikit)
+(icons uk/uikit)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(def ^:dynamic *uk-icon* "")
-(def ^:dynamic *icon*    nil)
-(def ^:dynamic *ratio*   nil)
+(defmulti uk-icon! h/kw-dispatcher :default ::default)
 
-(defmethod hl/do! :uk-icon
-  [elem _ v]
+(defmethod h/do! ::default
+  [elem key val]
+  (uk-icon! elem key val))
+
+(defmethod uk-icon! ::default
+  [elem kw v]
   (.icon uikit elem (clj->js v)))
 
-(hl/defelem icon [attr kids]
+(h/defelem icon [attr kids]
   (let [icon (:uk-icon attr (select-keys attr [:icon :ratio]))
         attr (assoc attr :uk-icon icon)]
-    (hl/span attr)))
-
-(hl/defelem link-icon [attr kids]
-  (let [icon (:uk-icon attr (select-keys attr [:icon :ratio]))
-        attr (-> attr
-              (assoc  :uk-icon icon)
-              (dissoc :icon :ratio))]
-    (hl/a (core/assoc-class attr {:uk-icon-link true}))))
-
-(hl/defelem button-icon [attr kids]
-  (let [icon (:uk-icon attr (select-keys attr [:icon :ratio]))
-        attr (-> attr
-              (assoc  :uk-icon icon)
-              (dissoc :icon :ratio))]
-    (hl/a (core/assoc-class attr {:uk-icon-button true}))))
-
-(hl/defelem image-icon [attr kids]
-  (let []
-    (hl/span (core/assoc-class attr {:uk-icon true :uk-icon-image true}) kids)))
+    (h/span attr)))
