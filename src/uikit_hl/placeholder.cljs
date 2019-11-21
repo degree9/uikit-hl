02b6bc5c70faa444315db13aac2b3cd60fa9a558
Nolan Jelley <nolanbjelley@gmail.com>
2019-11-20 22:37:51 -0700

    Update placeholder.cljs

--- src/uikit_hl/placeholder.cljs
+++ src/uikit_hl/placeholder.cljs
@@ -1,9 +1,22 @@
 (ns uikit-hl.placeholder
   (:require [hoplon.core :as hl]))
 
-(defmethod hl/do! :uk-placeholder
+(defmulti uk-placeholder! h/kw-dispatcher :default ::default)
+
+(defmethod h/do! ::default
+  [elem key val]
+  (uk-placeholder! elem key val))
+
+(defn- format-placeholder [placeholder]
+  (str "uk-placeholder-" placeholder))
+
+(defmethod uk-placeholder! ::default
+  [elem kw v]
+  (h/do! elem :class {(format-placeholder (name kw)) v}))
+
+(defmethod h/do! ::placeholder
   [elem _ v]
-  (hl/do! elem :class/uikit {:uk-placeholder v}))
+  (h/do! elem :class {:uk-placeholder v}))
 
 (hl/defelem placeholder [attr kids]
   (hl/div attr :uk-placeholder true kids))
