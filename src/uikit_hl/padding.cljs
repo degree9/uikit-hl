6b9af6c115bcd956d5c61211aefa020760aed579
Nolan Jelley <nolanbjelley@gmail.com>
2019-11-20 22:35:32 -0700

    Update padding.cljs

--- src/uikit_hl/padding.cljs
+++ src/uikit_hl/padding.cljs
@@ -1,79 +1,19 @@
 (ns uikit-hl.padding
-  (:require [hoplon.core :as hl]
-            [uikit-hl.core :as core]))
+  (:require [hoplon.core :as h]))
 
-(def ^:dynamic *padding* nil)
+(defmulti uk-padding! h/kw-dispatcher :default ::default)
 
-(def ^:dynamic *padding-small*  nil)
-(def ^:dynamic *padding-large*  nil)
-(def ^:dynamic *padding-xlarge* nil)
+(defmethod h/do! ::default
+  [elem key val]
+  (uk-padding! elem key val))
 
-(def ^:dynamic *padding-remove*           nil)
-(def ^:dynamic *padding-remove-top*       nil)
-(def ^:dynamic *padding-remove-bottom*    nil)
-(def ^:dynamic *padding-remove-left*      nil)
-(def ^:dynamic *padding-remove-right*     nil)
-(def ^:dynamic *padding-remove-vertical*  nil)
-(def ^:dynamic *padding-remove-horizontal*  nil)
+(defn- format-padding [padding]
+  (str "uk-padding-" padding))
 
-(defmethod hl/do! :uk-padding
-  [elem _ v]
-  (->> v
-    (merge {:uk-padding        *padding*
-            :uk-padding-small  *padding-small*
-            :uk-padding-large  *padding-large*
-            :uk-padding-xlarge *padding-xlarge*
-
-            :uk-padding-remove            *padding-remove*
-            :uk-padding-remove-top        *padding-remove-top*
-            :uk-padding-remove-bottom     *padding-remove-bottom*
-            :uk-padding-remove-left       *padding-remove-left*
-            :uk-padding-remove-right      *padding-remove-right*
-            :uk-padding-remove-vertical   *padding-remove-vertical*
-            :uk-padding-remove-horizontal *padding-remove-horizontal*})
-
-    (hl/do! elem :class)))
-
-(defmethod hoplon.core/do! :padding
+(defmethod uk-padding! ::default
   [elem kw v]
-  (hoplon.core/do! elem :uk-padding {:uk-padding v}))
+  (h/do! elem :class {(format-padding (name kw)) v}))
 
-(defmethod hoplon.core/do! :padding-small
+(defmethod uk-padding! ::padding
   [elem kw v]
-  (hoplon.core/do! elem :uk-padding {:uk-padding-small v}))
-
-(defmethod hoplon.core/do! :padding-large
-  [elem kw v]
-  (hoplon.core/do! elem :uk-padding {:uk-padding-large v}))
-
-(defmethod hoplon.core/do! :padding-xlarge
-  [elem kw v]
-  (hoplon.core/do! elem :uk-padding {:uk-padding-xlarge v}))
-
-(defmethod hoplon.core/do! :padding-remove
-  [elem kw v]
-  (hoplon.core/do! elem :uk-padding {:uk-padding-remove v}))
-
-(defmethod hoplon.core/do! :padding-remove-top
-  [elem kw v]
-  (hoplon.core/do! elem :uk-padding {:uk-padding-remove-top v}))
-
-(defmethod hoplon.core/do! :padding-remove-bottom
-  [elem kw v]
-  (hoplon.core/do! elem :uk-padding {:uk-padding-remove-bottom v}))
-
-(defmethod hoplon.core/do! :padding-remove-left
-  [elem kw v]
-  (hoplon.core/do! elem :uk-padding {:uk-padding-remove-left v}))
-
-(defmethod hoplon.core/do! :padding-remove-right
-  [elem kw v]
-  (hoplon.core/do! elem :uk-padding {:uk-padding-remove-right v}))
-
-(defmethod hoplon.core/do! :padding-remove-vertical
-  [elem kw v]
-  (hoplon.core/do! elem :uk-padding {:uk-padding-remove-vertical v}))
-
-(defmethod hoplon.core/do! :padding-remove-horizontal
-  [elem kw v]
-  (hoplon.core/do! elem :uk-padding {:uk-padding-remove-horizontal v}))
+  (h/do! elem :class {:uk-padding v}))
