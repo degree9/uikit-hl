de19d6f78a55ffafec89371058e44d13bd265399
Matthew Ratzke <matt@degree9.io>
2019-11-21 14:26:01 -0700

    Update pagination.cljs

--- src/uikit_hl/pagination.cljs
+++ src/uikit_hl/pagination.cljs
@@ -1,4 +1,38 @@
 (ns uikit-hl.pagination
-  (:require [hoplon.core :as h]
-            [hoplon.jquery]
-            [uikit-hl.core :as uk]))
+  (:require [hoplon.core :as h]))
+
+(defmulti uk-pagination! h/kw-dispatcher :default ::default)
+
+(defmethod h/do! ::default
+  [elem key val]
+  (uk-pagination! elem key val))
+
+(defn- format-pagination [pagination]
+  (str "uk-pagination-" pagination))
+
+(defmethod uk-pagination! ::default
+  [elem kw v]
+  (h/do! elem :class {(format-pagination (name kw)) v}))
+
+(defmethod uk-pagination! ::pagination
+  [elem kw v]
+  (h/do! elem :class {:uk-pagination v}))
+
+(defmethod uk-pagination! ::active
+  [elem kw v]
+  (h/do! elem :class {:uk-active v}))
+
+(defmethod uk-pagination! ::disabled
+  [elem kw v]
+  (h/do! elem :class {:uk-disabled v}))
+
+(defmethod uk-pagination! ::next
+  [elem kw v]
+  (h/do! elem :uk-pagination-next v))
+
+(defmethod uk-pagination! ::previous
+  [elem kw v]
+  (h/do! elem :uk-pagination-previous v))
+
+(h/defelem pagination [attr kids]
+  (h/ul attr ::pagination true kids))
