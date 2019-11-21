6cf11de203d4d2c5f75dc886295442a738547ddc
Nolan Jelley <nolanbjelley@gmail.com>
2019-11-20 18:54:33 -0700

    Update align.cljs

--- src/uikit_hl/align.cljs
+++ src/uikit_hl/align.cljs
@@ -1,46 +1,14 @@
 (ns uikit-hl.align
-  (:require [hoplon.core :as hl]))
+  (:require [clojure.string :as s]
+            [hoplon.core :as h]
+            [hoplon.jquery]))
 
-(defmethod hl/do! :uk-align-left
-  [elem _ v]
-  (hl/do! elem :class/uikit {:uk-align-left v}))
+(defn- format-align [align]
+  (-> (str "uk-align-" align)
+    (s/replace "-s" "@s")
+    (s/replace "-m" "@m")
+    (s/replace "-l" "@l")))
 
-(defmethod hl/do! :uk-align-left-s
-  [elem _ v]
-  (hl/do! elem :class/uikit {(keyword "uk-align-left@s") v}))
-
-(defmethod hl/do! :uk-align-left-m
-  [elem _ v]
-  (hl/do! elem :class/uikit {(keyword "uk-align-left@m") v}))
-
-(defmethod hl/do! :uk-align-left-l
-  [elem _ v]
-  (hl/do! elem :class/uikit {(keyword "uk-align-left@l") v}))
-
-(defmethod hl/do! :uk-align-left-xl
-  [elem _ v]
-  (hl/do! elem :class/uikit {(keyword "uk-align-left@xl") v}))
-
-(defmethod hl/do! :uk-align-right
-  [elem _ v]
-  (hl/do! elem :class/uikit {:uk-align-right v}))
-
-(defmethod hl/do! :uk-align-right-s
-  [elem _ v]
-  (hl/do! elem :class/uikit {(keyword "uk-align-right@s") v}))
-
-(defmethod hl/do! :uk-align-right-m
-  [elem _ v]
-  (hl/do! elem :class/uikit {(keyword "uk-align-right@m") v}))
-
-(defmethod hl/do! :uk-align-right-l
-  [elem _ v]
-  (hl/do! elem :class/uikit {(keyword "uk-align-right@l") v}))
-
-(defmethod hl/do! :uk-align-right-xl
-  [elem _ v]
-  (hl/do! elem :class/uikit {(keyword "uk-align-right@xl") v}))
-
-(defmethod hl/do! :uk-align-center
-  [elem _ v]
-  (hl/do! elem :class/uikit {:uk-align-center v}))
+(defmethod h/do! ::default
+  [elem kw v]
+  (h/do! elem :class {(format-align (name kw)) v}))
